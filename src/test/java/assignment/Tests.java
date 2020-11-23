2706cd6cccded3050cf6b43be22b3c00b8a07533
Zack Osowski <62389933+zackosowski@users.noreply.github.com>
2020-11-23 09:30:43 -0600

    Initial commit

---
+++ src/test/java/assignment/Tests.java
@@ -0,0 +1,22 @@
+package assignment;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+//import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.params.ParameterizedTest;
+//import org.junit.jupiter.params.provider.CsvSource;
+
+//import java.io.*;
+
+
+public class Tests {
+
+
+   @Test
+   public void test(){
+       App.main(null);
+       //assertEquals("EXPECTED", App.testMethod(), "ERROR MESSAGE");
+   }
+
+}
